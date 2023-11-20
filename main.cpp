1c46e22da30f949ecbc5c524d01af325f3ebc1af
PapiVPG <pawlikenemy@wp.pl>
2023-11-20 23:11:28 +0100

    update tests

--- main.cpp
+++ main.cpp
@@ -10,10 +10,12 @@ TEST( VectorTest, initialization )
 {
 	Vector< int > vector{ 1, 3, 4, 5, 7 };
 	Vector< int > vector_2( SIZE );
+	Vector< int > vector_3;
 	
 	EXPECT_EQ( vector.size(), SIZE );
 	ASSERT_FALSE( vector.empty() );
-	ASSERT_TRUE( vector_2.empty() );
+	ASSERT_FALSE( vector_2.empty() );
+	ASSERT_TRUE( vector_3.empty() );
 }
 
 TEST( VectorTest, operation_on_vector )
@@ -22,16 +24,18 @@ TEST( VectorTest, operation_on_vector )
 
 	const int TEST_VALUE = 5;
 	vector.push_back( TEST_VALUE );
-	EXPECT_EQ( vector[ 0 ], TEST_VALUE );
+	EXPECT_EQ( vector[ 0 ], 0 );
+	EXPECT_EQ( vector[ SIZE ], TEST_VALUE );
 
-	vector[ 0 ] = 68;
-	EXPECT_EQ( vector[ 0 ], 68 );
+	const int TEST_VALUE_2 = 68;
+	vector[ 0 ] = TEST_VALUE_2;
+	EXPECT_EQ( vector[ 0 ], TEST_VALUE_2 );
 
 	vector.push_back( TEST_VALUE * 2 );
-	EXPECT_EQ( vector[ 1 ], TEST_VALUE * 2 );
+	EXPECT_EQ( vector[ SIZE + 1 ], TEST_VALUE * 2 );
 
 	vector.pop_back();
-	EXPECT_EQ( vector.size(), 1 );
+	EXPECT_EQ( vector.size(), 6 );
 
 	vector.clear();
 	EXPECT_EQ( vector.size(), 0 );
@@ -45,27 +49,28 @@ TEST( VectorTest, constructor_tests )
 	vector.push_back( TEST_VALUE );
 	vector[ 4 ] = TEST_VALUE_2;
 	Vector< int > vector_2( vector );
-	EXPECT_EQ( vector_2[ 0 ], TEST_VALUE );
 	EXPECT_EQ( vector_2[ 4 ], TEST_VALUE_2 );
+	EXPECT_EQ( vector_2[ SIZE ], TEST_VALUE );
 
 	Vector< int > vector_3( std::move( vector ) );
-	EXPECT_EQ( vector_3[ 0 ], TEST_VALUE );
 	EXPECT_EQ( vector_3[ 4 ], TEST_VALUE_2 );
+	EXPECT_EQ( vector_3[ SIZE ], TEST_VALUE );
 	EXPECT_EQ( vector.size(), 0 );
 
 	vector_3.push_back( TEST_VALUE * 3 );
 	vector = vector_3;
-	EXPECT_EQ( vector[ 0 ], TEST_VALUE );
-	EXPECT_EQ( vector[ 1 ], TEST_VALUE * 3 );
 	EXPECT_EQ( vector[ 4 ], TEST_VALUE_2 );
+	EXPECT_EQ( vector[ SIZE ], TEST_VALUE );
+	EXPECT_EQ( vector[ SIZE + 1 ], TEST_VALUE * 3 );
 
 	vector_3.push_back( TEST_VALUE * 4 );
 	vector_2 = std::move( vector_3 );
-	EXPECT_EQ( vector_2[ 0 ], TEST_VALUE );
-	EXPECT_EQ( vector_2[ 1 ], TEST_VALUE * 3 );
-	EXPECT_EQ( vector_2[ 2 ], TEST_VALUE * 4 );
 	EXPECT_EQ( vector_2[ 4 ], TEST_VALUE_2 );
+	EXPECT_EQ( vector_2[ SIZE ], TEST_VALUE );
+	EXPECT_EQ( vector_2[ SIZE + 1 ], TEST_VALUE * 3 );
+	EXPECT_EQ( vector_2[ SIZE + 2 ], TEST_VALUE * 4 );
 }
+
 
 int main( int argc, char** argv )
 {
