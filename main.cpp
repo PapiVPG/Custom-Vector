eb7dd9301b23b1803c4d23640e8ba98bd757af5b
PapiVPG <pawlikenemy@wp.pl>
2023-11-20 22:25:50 +0100

    update tests

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ const size_t SIZE = 5;
 
 TEST( VectorTest, initialization )
 {
-	Vector< int > vector( SIZE );
-	Vector< int > vector_2;
+	Vector< int > vector{ 1, 3, 4, 5, 7 };
+	Vector< int > vector_2( SIZE );
 	
 	EXPECT_EQ( vector.size(), SIZE );
 	ASSERT_FALSE( vector.empty() );
@@ -24,12 +24,14 @@ TEST( VectorTest, operation_on_vector )
 	vector.push_back( TEST_VALUE );
 	EXPECT_EQ( vector[ 0 ], TEST_VALUE );
 
+	vector[ 0 ] = 68;
+	EXPECT_EQ( vector[ 0 ], 68 );
+
 	vector.push_back( TEST_VALUE * 2 );
 	EXPECT_EQ( vector[ 1 ], TEST_VALUE * 2 );
-	EXPECT_EQ( vector.capacity(), 2 );
 
 	vector.pop_back();
-	EXPECT_EQ( vector.size(), 4 );
+	EXPECT_EQ( vector.size(), 1 );
 
 	vector.clear();
 	EXPECT_EQ( vector.size(), 0 );
@@ -54,15 +56,15 @@ TEST( VectorTest, constructor_tests )
 	vector_3.push_back( TEST_VALUE * 3 );
 	vector = vector_3;
 	EXPECT_EQ( vector[ 0 ], TEST_VALUE );
+	EXPECT_EQ( vector[ 1 ], TEST_VALUE * 3 );
 	EXPECT_EQ( vector[ 4 ], TEST_VALUE_2 );
-	EXPECT_EQ( vector[ 5 ], TEST_VALUE * 3 );
 
 	vector_3.push_back( TEST_VALUE * 4 );
 	vector_2 = std::move( vector_3 );
 	EXPECT_EQ( vector_2[ 0 ], TEST_VALUE );
+	EXPECT_EQ( vector_2[ 1 ], TEST_VALUE * 3 );
+	EXPECT_EQ( vector_2[ 2 ], TEST_VALUE * 4 );
 	EXPECT_EQ( vector_2[ 4 ], TEST_VALUE_2 );
-	EXPECT_EQ( vector_2[ 5 ], TEST_VALUE * 3 );
-	EXPECT_EQ( vector_2[ 6 ], TEST_VALUE * 4 );
 }
 
 int main( int argc, char** argv )
